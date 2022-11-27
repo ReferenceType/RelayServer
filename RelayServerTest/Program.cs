2e423645da05530abd4cc12e17ae2c0c6bbec655
ReferenceType <dcan.ozturk@gmail.com>
2022-11-27 02:48:43 +0100

    added proxy update routine

--- RelayServerTest/Program.cs
+++ RelayServerTest/Program.cs
@@ -1,27 +1,70 @@
 ﻿using NetworkLibrary.Components;
+using NetworkLibrary.Components.Statistics;
 using ProtoBuf;
 using Protobuff;
 using Protobuff.P2P;
+using System;
 using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
 
 namespace RrlayServerTest
 {
     //KFtyEu7h6E
-    internal class Program
+    public class Adress
     {
-        [ProtoContract]
-        public class PeerList
-        {
-            [ProtoMember(1)]
-            public Guid asd;
+        public string ip { get; set; }
+        public string country { get; set; }
+        public string cc { get; set; }
+    }
 
+    public class UpdateAdress
+    {
+        public string Ip { get; set; }
 
-        }
+        public string Id { get; set; }
+    }
+
+    internal class Program
+        
+    {
+        private static readonly string uri = @"http://localhost:8001/";
+        static ManualResetEvent m = new ManualResetEvent(false);
+       
         static void Main(string[] args)
         {
            
             var scert = new X509Certificate2("server.pfx", "greenpass");
             var server = new SecureProtoRelayServer(20011, 1000, scert);
+            Task.Run(async () => {
+                while (true)
+                {
+                    try
+                    {
+                        HttpClient cl = new HttpClient();
+
+                        var uri_ = @"https://api.myip.com";
+                        var result = await cl.GetAsync(uri_);
+                        var str = await result.Content.ReadAsStringAsync();
+
+                        var IPinfo = JsonSerializer.Deserialize<Adress>(str);
+                        var update = new UpdateAdress()
+                        {
+                            Ip = IPinfo.ip,
+                            Id = "IPass"
+                        };
+
+                        var updateMsg = JsonSerializer.Serialize(update);
+                        await cl.PostAsync(uri, new StringContent(updateMsg));
+                        await Task.Delay(60000);
+                    }
+                    catch (Exception ex) { }
+                   
+
+                }
+
+
+
+            });
             Task.Run(async () =>
             {
                 while (true)
@@ -29,16 +72,18 @@ namespace RrlayServerTest
                     await Task.Delay(1000);
                     server.GetTcpStatistics(out SessionStats generalStats, out var a);
                     server.GetUdpStatistics(out UdpStatistics generalStatsU, out _);
-                    (int left,int right )=Console.GetCursorPosition();
+                    (int left, int right) = Console.GetCursorPosition();
                     Console.Clear();
-                    Console.WriteLine("Session Count: "+a.Count);
+                    Console.WriteLine("Session Count: " + a.Count);
                     Console.WriteLine(generalStats.ToString());
                     Console.WriteLine(generalStatsU.ToString());
-                    if(left != 0 && right != 0)
+                    if (left != 0 && right != 0)
                         Console.SetCursorPosition(left, right);
                 }
 
             });
+
+            m.WaitOne();
             while (Console.ReadLine() != "e")
             {
 
